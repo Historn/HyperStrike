bbbccd3784ca6b4db3f03b4a6438dbc31b79b859
Adria Pons <adria.pons.mensa@estudiantat.upc.edu>
2025-06-14 18:24:06 +0200

    Added PlayFootstepSound.cs + Tags Assignation
    - Added PlayFootstepSound.cs script.
    - Added AudioSource + Script components to Crashwall, Nanoflow, and Speed character prefabs.
    - Added corresponding Tags to each environment asset (Grass, Metal, or Tile).

---
+++ HyperStrike/Assets/Scripts/Player/PlayFootstepSound.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using TMPro;
+
+public class PlayFootstepSound : MonoBehaviour
+{
+    public AudioClip[] footstepsOnGrass;
+    public AudioClip[] footstepsOnMetal;
+    public AudioClip[] footstepsOnTile;
+
+    private AudioSource myAudioSource;
+    private string material;
+
+    void Start()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    void WhenPlayingFootstepSound()
+    {
+        myAudioSource.volume = Random.Range(0.8f, 1.0f);
+        myAudioSource.pitch = Random.Range(0.8f, 1.0f);
+
+        switch (material)
+        {
+            case "Grass":
+                myAudioSource.PlayOneShot(footstepsOnGrass[Random.Range(0, footstepsOnGrass.Length)]);
+                break;
+
+            case "Metal":
+                myAudioSource.PlayOneShot(footstepsOnMetal[Random.Range(0, footstepsOnMetal.Length)]);
+                break;
+            
+            case "Tile":
+                myAudioSource.PlayOneShot(footstepsOnTile[Random.Range(0, footstepsOnTile.Length)]);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Ground":
+            case "Wood":
+            case "Floor":
+            case "Rock":
+            case "Metal":
+            case "Water":
+                material = collision.gameObject.tag;
+                WhenPlayingFootstepSound();
+                break;
+
+            default:
+                break;
+        }
+    }
+}
